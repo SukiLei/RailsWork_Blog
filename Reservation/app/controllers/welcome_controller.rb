b781a96817d5dba2dbe5f29653a5d3e7afde45e8
SukiLei <lyyolive@qq.com>
2015-02-17 12:19:58 +0800

    UPDATE RESERVATION
    UPDATE RESERVATION

--- Reservation/app/controllers/welcome_controller.rb
+++ Reservation/app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     #https://www.gallupstrengthscenter.com/SignIn/GSC/Index?ReturnUrl=%2fPrivate%2fen-US%2fDashboard#
+    #https://www.gallupstrengthscenter.com/Purchase/en-US/Product
   end
 end
